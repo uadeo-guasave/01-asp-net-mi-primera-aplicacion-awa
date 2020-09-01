3055ea0d987e8e739b971b9eb5eb7cc9e5174e8c
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-09-01 15:24:22 -0600

    03. Extender el proyecto
    EFCore Sqlite
    Modelo alumno
    DB en Sqlite

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _01_asp_net_mi_primera_aplicacion.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -10,31 +11,43 @@ using Microsoft.Extensions.Hosting;
 
 namespace _01_asp_net_mi_primera_aplicacion
 {
-    public class Startup
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hola Mundo!");
-                });
-            });
-        }
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseRouting();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapGet("/", async context =>
+              {
+                await context.Response.WriteAsync("Hola Mundo!");
+              });
+
+        endpoints.MapGet("alumnos", async context =>
+        {
+          var alumno = new Alumno
+          {
+              Id=1,
+              Nombre="Karen",
+              Apellidos="Estrada",
+              CorreoElectronico="karen@estrada.com"
+          };
+          await context.Response.WriteAsync(alumno.ToString());
+        });
+      });
+    }
+  }
 }
